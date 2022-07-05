28b277261c5065e29cbb1af9fef2a959e2d06a01
thapaRoyal <thaparoyal17@gmail.com>
2022-07-05 15:53:53 +0545

    implementing  dynamic column in masonry list

--- components/MasonryList.tsx
+++ components/MasonryList.tsx
@@ -1,4 +1,10 @@
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  useWindowDimensions,
+} from 'react-native';
 
 import Pin from '../components/Pin';
 
@@ -11,26 +17,22 @@ interface IMasonryList {
 }
 
 const MasonryList = ({ pins }: IMasonryList) => {
+  const width = useWindowDimensions().width;
+
+  const numColumns = Math.ceil(width / 300);
+
   return (
     <ScrollView>
       <View style={styles.container}>
-        {/* 1st column */}
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 0)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
-
-        {/* 2nd column */}
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 1)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
+        {Array.from(Array(numColumns)).map((_, colIndex) => (
+          <View style={styles.column}>
+            {pins
+              .filter((_, index) => index % numColumns === colIndex)
+              .map((pin) => (
+                <Pin pin={pin} key={pin.id} />
+              ))}
+          </View>
+        ))}
       </View>
     </ScrollView>
   );
