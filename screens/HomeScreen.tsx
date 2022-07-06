dd8157eafdd53d1f189224d291c4a25e72c84394
thapaRoyal <thaparoyal17@gmail.com>
2022-07-06 15:42:01 +0545

    query

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,30 @@
+import { useNhostClient } from '@nhost/react';
+import { useEffect } from 'react';
 import pins from '../assets/data/pins';
 import MasonryList from '../components/MasonryList';
 
 export default function HomeScreen() {
+  const nhost = useNhostClient();
+  const fetchPins = async () => {
+    const response = await nhost.graphql.request(
+      `
+      query MyQuery {
+        pins {
+          user_id
+          title
+          image
+          id
+          created_at
+        }
+      }
+      `
+    );
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchPins();
+  }, []);
+
   return <MasonryList pins={pins} />;
 }
