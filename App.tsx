0737ed7d4fd89bb40352b6407d633690d6fab4e0
thapaRoyal <thaparoyal17@gmail.com>
2022-07-06 14:58:38 +0545

    initialize nhost

--- App.tsx
+++ App.tsx
@@ -4,6 +4,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { NhostClient, NhostReactProvider } from '@nhost/react';
+import * as SecureStore from 'expo-secure-store';
+
+const nhost = new NhostClient({
+  backendUrl: 'https://wcjpxgetfdmwvatkncub.nhost.run',
+  clientStorageType: 'expo-secure-storage',
+  clientStorage: SecureStore,
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -13,10 +21,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NhostReactProvider nhost={nhost}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </NhostReactProvider>
     );
   }
 }
--- components/MasonryList.tsx
+++ components/MasonryList.tsx
@@ -25,7 +25,7 @@ const MasonryList = ({ pins }: IMasonryList) => {
     <ScrollView>
       <View style={styles.container}>
         {Array.from(Array(numColumns)).map((_, colIndex) => (
-          <View style={styles.column}>
+          <View style={styles.column} key={`column_${colIndex}`}>
             {pins
               .filter((_, index) => index % numColumns === colIndex)
               .map((pin) => (
